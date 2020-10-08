2ee0f1c583f664ea04ffa965a651acc8328b1267
Romeu Gomes <romeu.tmccomunicacao@gmail.com>
2020-10-08 12:16:52 -0300

    Initial Commint

---
+++ resources/_top.php
@@ -0,0 +1,7 @@
+﻿<?php
+// Abre a conexão com o SQLite3
+
+// Faz o ROTEAMENTO conforme a ação requerida
+
+//Cria as funções de usuário da base
+?>
---
+++ resources/main.php
@@ -0,0 +1,15 @@
+<?php include_once('_top.php'); ?>
+<!DOCTYPE html>
+<html lang="pt_br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Jukefy - The Jukebox</title>
+    <link rel="stylesheet" href="css/bootstrap.css"crossorigin="anonymous">
+    <script src="js/bootstrap.js" crossorigin="anonymous"></script>
+</head>
+<body>
+<?php require_once('menu.php'); ?>
+</body>
+</html>
+<?php include_once('_bottom.php'); ?>
