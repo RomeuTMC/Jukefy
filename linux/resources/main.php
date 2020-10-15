c2fc5f707004fbae159f8285d4e25c79b19cbfa6
romeu <romeu.tmccomunicacao@gmail.com>
2020-10-14 22:10:50 -0300

    v0.0.1-test

--- linux/resources/main.php
+++ linux/resources/main.php
@@ -1,15 +1,65 @@
-<?php include_once('_top.php'); ?>
 <!DOCTYPE html>
 <html lang="pt_br">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Jukefy - The Jukebox</title>
-    <link rel="stylesheet" href="css/bootstrap.css"crossorigin="anonymous">
-    <script src="js/bootstrap.js" crossorigin="anonymous"></script>
 </head>
+<style>
+p{ 
+    text-align:justify;
+    font-size: 15px;
+}
+h1,h2,h3{
+    text-align:left;
+    color:brown;
+}
+h1:after,h2:after {
+    content:'';
+    display:block;
+    border:1px solid #000;
+}
+</style>
 <body>
-<?php require_once('menu.php'); ?>
+<h1>Jukefy a Jukebox Free</h1>
+<p>O projeto jukefy, foi idealizado como um projeto acadêmico onde seria 
+possível a integração de diversas tecnologias móveis, desktop, e cliente / 
+servidor. <br>
+Através dele um estabelescimento comercial como bar, restaurante, ou mesmo 
+em uma festa, basta ter instalado o ambiente em um computador e liga-lo ao 
+sistema de som, e pronto, os demais usuários que possuirem a senha de 
+acesso podem enfileirar suas músicas que serão executadas pela ordem em que
+foram solicitadas, e respeitando a prioridade dos demais usuários.<br>
+Também, é possivel ao anfitrião configurar o tempo para começar a executar 
+as músicas de maneira aleatória e o tempo para executar arquivos específicos
+de propagandas, ou mesmo informar a data e hora certos.<br>
+ </p>
+<h2>Instalação</h2>
+<h3>Linux e outros sistemas UNIX/POSIX</h3>
+<p>Para executar sobre o ambiente <b>Linux Autonomo</b> basta copiar todos 
+os arquivos da pasta <strong>/src</strong> para a pasta <strong>/linux/resources</strong> 
+substituindo todo o conteúdo.</p>
+<p>Para executar em sistema unix/posix (linux e bsd) basta executar o arquivo
+<strong>jukefy</strong> que se encontra dentro da pasta linux. As versões 
+RC, BETA e FINAIS basta baixar o arquivo, descompactar para o diretório 
+desejado e executar o arquivo Jukefy.</p>
+
+<h3>Windows XP, Windows 7, Windows 8, Windows 10</h3>
+<p>Para executar sobre o ambiente <b>Windows Autonomo</b> basta copiar todos 
+os arquivosda pasta <strong>/src</strong> para a pasta <strong>/windows/resources</strong> 
+substituindo todo o conteúdo.</p>
+<p>Para executar em sistema windows (a partir do XP ou mais novo) basta 
+executar o arquivo <strong>jukefy.exe</strong> que se encontra dentro da 
+pasta windows. As versões RC, BETA e FINAIS possuem um instalador para 
+windows, totalmente independentes.</p>
+
+<h3>MacOs</h3>
+<p>Não tem uma versão específica para o sistema MacOS, para executa-lo sobre 
+este ambiente, você pode optar por utilizar a versão <b>LAMPP/XAMPP/MAMPP</b> 
+para instalar, e executar, siga os passos abaixo.</p>
+
+<h3>XAMPP (windows servidor) - LAMPP (linux servidor) - MAMPP (MacOs todos)</h3>
+<p>Primeiro baixe a versão correta do ambiente para o seu sistema operacional em:<br>
+<a href="https://www.apachefriends.org/pt_br/index.html">
 </body>
 </html>
-<?php include_once('_bottom.php'); ?>
